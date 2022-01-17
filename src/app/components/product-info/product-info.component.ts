e32da8811b33af969327d792c56cd085a50460b2
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-17 13:42:58 +0100

    Refactor product-info

--- src/app/components/product-info/product-info.component.ts
+++ src/app/components/product-info/product-info.component.ts
@@ -6,39 +6,38 @@ import { NotificationService } from 'src/app/services/notification.service';
 @Component({
   selector: 'app-product-info',
   templateUrl: './product-info.component.html',
-  styleUrls: ['./product-info.component.scss']
+  styleUrls: ['./product-info.component.scss'],
 })
 export class ProductInfoComponent implements OnInit {
-
-  public dataService: CartService;
-  public notificationService: NotificationService;
-
-  constructor(private service: CartService, private secondService: NotificationService) {
-    this.dataService = this.service;
-    this.notificationService = this.secondService;
-  }
+  constructor(
+    public cartService: CartService,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit(): void {}
 
   public getStyle(color: string, selected: boolean): string {
     if (selected) {
-      return "";
+      return '';
     }
   }
 
   public addItemToCart() {
-    this.dataService.addItemToCart([{
-      sku: '50433323',
-      desc: 'In Italien gefertigte Sneakers aus Leder mit Monogramm-Details',
-      imgSrc: 'https://images.hugoboss.com/is/image/boss/hbeu50410989_001_250?$re_fullPageZoom$&qlt=85&wid=300&hei=200',
-      price: new Decimal('250.00'), // Price
-      taxType: 'vat_standard',
-      weight: 610, // grams
-      height: 4.20, // cm
-      width: 27.5, // cm
-      depth: 6.22, // cm
-      quantity: 1,
-    }]);
+    this.cartService.addItemToCart([
+      {
+        sku: '50433323',
+        desc: 'In Italien gefertigte Sneakers aus Leder mit Monogramm-Details',
+        imgSrc:
+          'https://images.hugoboss.com/is/image/boss/hbeu50410989_001_250?$re_fullPageZoom$&qlt=85&wid=300&hei=200',
+        price: new Decimal('250.00'), // Price
+        taxType: 'vat_standard',
+        weight: 610, // grams
+        height: 4.2, // cm
+        width: 27.5, // cm
+        depth: 6.22, // cm
+        quantity: 1,
+      },
+    ]);
     this.notificationService.success();
   }
 }
