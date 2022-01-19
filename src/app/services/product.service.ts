b098ce616a04959d11782b54a644949229016d49
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-19 14:15:41 +0100

    Load products data with services in shoes

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Product } from '../models/product';
+import { Product as ProductCard } from '../pages/products/products.component';
 
 import { SHOES } from '../data/products-shoes';
+import { products } from '../data/product-samples';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +15,9 @@ export class ProductService {
   getShoes(): Observable<Product[]> {
     const shoes = of(SHOES);
     return shoes;
+  }
+
+  getProducts(): Observable<ProductCard[]> {
+    return of(products);
   }
 }
