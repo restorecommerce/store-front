38189728da356b0bac97de2fba53e8d3c8a88b1b
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-20 10:28:45 +0100

    Pass and fetch the product gallery images

--- src/app/components/product-gallery/product-gallery.component.ts
+++ src/app/components/product-gallery/product-gallery.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit, Input, AfterViewInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
-import { CartService } from 'src/app/services/cart.service';
+import {
+  Component,
+  OnInit,
+  Input,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  AfterViewChecked,
+} from '@angular/core';
+import { Product, ProductImage } from 'src/app/models/product';
 
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-gallery',
   templateUrl: './product-gallery.component.html',
-  styleUrls: ['./product-gallery.component.scss']
+  styleUrls: ['./product-gallery.component.scss'],
 })
 export class ProductGalleryComponent implements OnInit {
   public loading = false;
@@ -14,19 +23,26 @@ export class ProductGalleryComponent implements OnInit {
   public dataService: CartService;
   public productZoomContainerWidth: number;
 
+  @Input() product: Product;
+
   constructor(private service: CartService) {
     this.dataService = this.service;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngOnChanges() {
     this.loading = true;
-    setTimeout(() => this.loading = false, 100)
+    setTimeout(() => (this.loading = false), 100);
+    this.setCurrentImageArray();
   }
 
   public zoom(): void {
     this.zoomImage = true;
+  }
+
+  setCurrentImageArray(): ProductImage[] {
+    const { productImageSources, selectedColor } = this.product;
+    return productImageSources[selectedColor];
   }
 }
