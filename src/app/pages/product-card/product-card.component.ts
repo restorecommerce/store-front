54a02bdfa9e773249ac677dd28ac56f27b9c98e4
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-24 14:11:46 +0100

    Add markup to the product card

--- src/app/pages/product-card/product-card.component.ts
+++ src/app/pages/product-card/product-card.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../products/products.component';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
+  product: Product;
 
-  constructor() { }
+  currency: string;
 
-  ngOnInit(): void {
-  }
+  responsiveImagesConfiguration = [
+    { width: 400, min: 0, max: 1100.99, suffix: '-xl', format: 'webp' },
+    { width: 400, min: 0, max: 1100.99, suffix: '-xl', format: 'jpeg' },
+    { width: 190, min: 1101, max: 1156.99, suffix: '-sm', format: 'webp' },
+    { width: 190, min: 1101, max: 1156.99, suffix: '-sm', format: 'jpeg' },
+    { width: 260, min: 1157, max: 1268.99, suffix: '-md', format: 'webp' },
+    { width: 260, min: 1157, max: 1268.99, suffix: '-md', format: 'jpeg' },
+    { width: 330, min: 1269, max: 1381.99, suffix: '-lg', format: 'webp' },
+    { width: 330, min: 1269, max: 1381.99, suffix: '-lg', format: 'jpeg' },
+    { width: 400, min: 1382, max: 99999, suffix: '-xl', format: 'webp' },
+    { width: 400, min: 1382, max: 99999, suffix: '-xl', format: 'jpeg' },
+  ];
 
+  constructor() {}
+
+  onChangeProductColor(color: string) {}
+
+  onProductClicked() {}
+
+  ngOnInit(): void {}
 }
--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -6,19 +6,6 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
-  responsiveImagesConfiguration = [
-    { width: 400, min: 0, max: 1100.99, suffix: '-xl', format: 'webp' },
-    { width: 400, min: 0, max: 1100.99, suffix: '-xl', format: 'jpeg' },
-    { width: 190, min: 1101, max: 1156.99, suffix: '-sm', format: 'webp' },
-    { width: 190, min: 1101, max: 1156.99, suffix: '-sm', format: 'jpeg' },
-    { width: 260, min: 1157, max: 1268.99, suffix: '-md', format: 'webp' },
-    { width: 260, min: 1157, max: 1268.99, suffix: '-md', format: 'jpeg' },
-    { width: 330, min: 1269, max: 1381.99, suffix: '-lg', format: 'webp' },
-    { width: 330, min: 1269, max: 1381.99, suffix: '-lg', format: 'jpeg' },
-    { width: 400, min: 1382, max: 99999, suffix: '-xl', format: 'webp' },
-    { width: 400, min: 1382, max: 99999, suffix: '-xl', format: 'jpeg' },
-  ];
-
   groups: { title: string; productCount: number }[] = [
     { title: 'all', productCount: 244 },
     { title: 'brand 1', productCount: 170 },
@@ -84,9 +71,9 @@ export class Product {
   id: string | number;
   title: string;
   price: number;
-  imgUrl: string;
   like: boolean;
   additionalLabel: string;
   colors: string[];
   selectedColor: string;
+  images: {};
 }
