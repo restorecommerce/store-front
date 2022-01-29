9750310259effdeaeab9917580f459d8fb83bafd
Bello Babakolo <fleetbeekay@gmail.com>
2022-01-29 12:57:48 +0100

    Add refactor cartLayer to cart-layer-contents

--- src/app/components/cart-layer-contents/cart-layer-contents.component.ts
+++ src/app/components/cart-layer-contents/cart-layer-contents.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TemplateLayerRef } from '@vcl/ng-vcl';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-layer-contents',
   templateUrl: './cart-layer-contents.component.html',
-  styleUrls: ['./cart-layer-contents.component.scss']
+  styleUrls: ['./cart-layer-contents.component.scss'],
 })
 export class CartLayerContentsComponent implements OnInit {
+  @Input() cartLayer: TemplateLayerRef;
 
-  constructor() { }
+  constructor(public cartService: CartService, public router: Router) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
