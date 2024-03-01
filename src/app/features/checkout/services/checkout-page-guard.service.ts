7774f14388235b28bfceb1bd046e91286aa5cb40
Bello Babakolo <fleetbeekay@gmail.com>
2024-03-01 17:19:34 +0100

    chore: update angular

--- src/app/features/checkout/services/checkout-page-guard.service.ts
+++ src/app/features/checkout/services/checkout-page-guard.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { CartService } from 'src/app/features/cart/services/cart.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CheckoutPageGuard implements CanActivate {
+export class CheckoutPageGuard  {
   constructor(private cartService: CartService, private router: Router) {}
 
   canActivate(
